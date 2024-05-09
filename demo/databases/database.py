e8ab32fddb4798a77873c789175df307f2c63814
Andrew Lee <ralphylee0729@gmail.com>
2024-05-09 18:59:48 -0400

    update database

--- demo/databases/database.py
+++ demo/databases/database.py
@@ -2,6 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+""" Valid SQLite URL forms are:
+ sqlite:///:memory: (or, sqlite://)
+ sqlite:///relative/path/to/file.db
+ sqlite:////absolute/path/to/file.db """
+
 engine = create_engine(
     "sqlite:///./demo.sqlite3",
     connect_args={"check_same_thread": False},
